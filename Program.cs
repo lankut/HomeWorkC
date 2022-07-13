d479a05a99656b0b70e4c71d39545d2bb1f38b56
lankut <108450370+lankut@users.noreply.github.com>
2022-07-13 13:21:43 +0300

    homwork add

--- Program.cs
+++ Program.cs
@@ -1,16 +1,14 @@
 ﻿//Задача 10: Напишите программу, которая принимает на вход 
 //трёхзначное число и на выходе показывает вторую цифру этого числа.
-/*
+
 int DigitNum(int num)
 {
-    int first = num / 100;
+    int first = num / 10;
     int second = first % 10;
     return second;
 }
-
 Console.Write("Введите трехзначное число: ");
 int number = Convert.ToInt32(Console.ReadLine());
-
 if (number > 99 && number<1000)
 {    
     int res = DigitNum(number);
@@ -22,22 +20,18 @@ else
 //Задача 13: Напишите программу, которая выводит
 //третью цифру заданного числа или сообщает, что третьей цифры нет.
 
-Console.Write("Введите число: ");
+Console.Write("Для вывода третьего символа любого числа, прошу наваяйте какое-нить число: ");
 int number = Convert.ToInt32(Console.ReadLine());
-
 int res = 0;
-
 while (number>999)
 {
     number = number/10;
     res = number%10;
 }
-
 if (number<=99)
     Console.WriteLine("Третьего числа не существует");
 else 
     Console.WriteLine(res);
-*/
 
 //Задача 15: Напишите программу, которая принимает на вход цифру,
 //обозначающую день недели, и проверяет, является ли этот день выходным.
